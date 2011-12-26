925702401ff500a37f0022507096a86040a2f58e
Vasil Vangelovski <vvangelovski@gmail.com>
2011-12-26 16:50:12 +0100

    admin

--- lamsondemo/webapp/forum/admin.py
+++ lamsondemo/webapp/forum/admin.py
@@ -1,2 +1,14 @@
-from django.contrib.admin import ModelAdmin
+from django.contrib.admin import ModelAdmin, site
 from models import Topic, Post, Subscription
+
+class TopicAdmin(ModelAdmin):
+    pass
+
+class SubscriptionAdmin(ModelAdmin):
+    pass
+
+
+
+site.register(Topic, TopicAdmin)
+site.register(Subscription, SubscriptionAdmin)
+
--- lamsondemo/webapp/forum/models.py
+++ lamsondemo/webapp/forum/models.py
@@ -4,14 +4,26 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     title = models.CharField(max_length = 255)
 
+    def __unicode__(self):
+        return self.title
+
 class Post(models.Model):
-    topic = models.ForeignKey(Post)
+    topic = models.ForeignKey(Topic)
     content = models.TextField()
+
+    def __unicode__(self):
+        return self.topic.title
 
 class Subscription(models.Model):
     subscriber = models.ForeignKey(User)
     topic = models.ForeignKey(Topic)
 
+    def __unicode__(self):
+        return "%s - %s"%(self.subscirber.username, topic.title)
+
 class ReplyAddress(models.Model):
     subscription = models.ForeignKey(Subscription)
     address = models.EmailField()
+
+    def __unicode__(self):
+        return self.address
